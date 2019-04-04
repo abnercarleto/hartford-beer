0a046810b2579407d69e492e9051f1cab4591c2c
Abner Lassalves Carleto <tqi_alassalves@uolinc.com>
2019-04-04 15:22:48 -0300

    Scope paid and not paid beers

--- app/admin/beers.rb
+++ app/admin/beers.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register Beer do
   permit_params :admin_user_id, :paid_out, :paid_date
+
+  scope :all, default: true
+  scope :paid_out
+  scope :not_paid_out
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
